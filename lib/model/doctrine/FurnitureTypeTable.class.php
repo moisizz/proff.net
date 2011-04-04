1cf716bbbc1623cb0c94f2beb30cacc3c6e8b3d4
Moisizz <moisizz@gmail.com>
2011-04-04 13:05:44 +0600

    Добавление метода получения списка типов мебели для конкретной комнаты

--- lib/model/doctrine/FurnitureTypeTable.class.php
+++ lib/model/doctrine/FurnitureTypeTable.class.php
@@ -16,4 +16,16 @@ class FurnitureTypeTable extends Doctrine_Table
     {
         return Doctrine_Core::getTable('FurnitureType');
     }
+    
+    public function getRoomFurnitureTypes($room_id)
+    {
+      $q = $this->createQuery('f')
+                ->where('f.id IN (SELECT frt.furniture_type_id FROM RoomFurnitureType frt WHERE frt.room_id = ?)', $room_id)
+                ->setHydrationMode(Doctrine_Core::HYDRATE_ARRAY);
+      
+      $furniture_list = $q->execute();
+      $q->free();
+      
+      return $furniture_list;
+    }
 }
