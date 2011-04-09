02df58b9fb67d9b8c8271073dce062ba5365122c
Moisizz <moisizz@gmail.com>
2011-04-09 21:47:44 +0600

    Добавление в класс пользователя методов создания/удаления предзаказа и добавления в него элементов (мебель, материал, сделанная ранее работа)

--- apps/frontend/lib/myUser.class.php
+++ apps/frontend/lib/myUser.class.php
@@ -2,4 +2,68 @@
 
 class myUser extends sfGuardSecurityUser
 {
+  /**
+   * 
+   * Защищенная функция для централизации добавления 
+   * в предзаказ элементов
+   * @param unknown_type $id
+   * @param unknown_type $unit_type
+   */
+  protected function addUnitToPreorder($id, $unit_type)
+  {
+    if ($this->hasAttribute('preorder')):
+      $preorder = $this->getAttribute('preorder');
+      $preorder[$unit_type][] = $id;   
+    else:
+      $this->setAttribute('preorder', array($unit_type => ($id)));
+    endif;
+  }
+  
+  /**
+   * 
+   * Добавление в предзаказ мебели
+   * @param unknown_type $id
+   */
+  public function addFurnitureToPreorder($id)
+  {
+    $this->addUnitToPreorder($id, 'furniture');
+  }
+  
+  /**
+   * 
+   * Добавление в предзаказ материала
+   * @param unknown_type $id
+   */
+  public function addMaterialToPreorder($id)
+  {
+    $this->addUnitToPreorder($id, 'material');
+  }
+  
+  /**
+   * 
+   * Добавление в предзаказ сделанной ранее работы
+   * @param unknown_type $id
+   */
+  public function addPortfolioToPreorder($id)
+  {
+    $this->addUnitToPreorder($id, 'portfolio');
+  }
+  
+  /**
+   * 
+   * Удаление предзаказа из сессии
+   */
+  protected function removePreorder()
+  {
+    if ($this->hasAttribute('preorder'))
+      $this->getAttributeHolder()->remove('preorder');
+  }
+  
+  public function signOut()
+  {
+    $this->removePreorder();
+    
+    parent::signOut();
+  }
 }
+
